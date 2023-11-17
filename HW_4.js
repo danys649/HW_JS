2e62b76952626380eb72c6dcb685dc091b38f0bf
danys649 <dfurkol653@gmail.com>
2023-11-17 19:16:35 +0200

    0.2

--- HW_4.js
+++ HW_4.js
@@ -15,49 +15,23 @@ if (number % 3 === 0 && number % 5 === 0) {
                              Завдання 2
 
 let value = prompt("Будь ласка, введіть будь-яке значення");
-let type;
-//перевірка на  буліан
-if (value.toLowerCase() === "false" || value.toLowerCase() === "true") {
-  type = "boolean";
-}
-//перевірка на число
-else if (!isNaN(value)) 
+let type = "string";
+let words = value.split(" ");                             
+for (let word of words) //перебираємо слова
 {
-  type = "number";
-} 
-//Перевірка на різність типів
-else 
-{
-  let words = value.split(" "); //Розбиваємо рядок на слова
-  for (let word of words) //перебираємо слова
+  if (!isNaN(word) ||  word.toLowerCase() === "false" || word.toLowerCase() === "true") 
   {
-    if (!isNaN(word) ||  word.toLowerCase() === "false" || word.toLowerCase() === "true") 
-    {
-      //Якщо введені дані різні за типом
-      type = "complex";
-      break;
-    }
+    type = "complex";
+    break;
   }
-  if (!type) 
+}  
+  if (!isNaN(value)) 
+  {
+    type = "number";
+  }
+   else if (value.toLowerCase() === "false" || value.toLowerCase() === "true") 
    {
-     //якщо type залишиться undefined (не було присвоєно значення)
-     type = "string";
-   }
-}
-//Вивід результатів
-switch (type) {
-  case "boolean":
-    console.log("You entered a boolean");
-    break;
-  case "number":
-    console.log("You entered a number");
-    break;
-  case "string":
-    console.log("You entered a string");
-    break;
-  case "complex":
-    console.log("You entered a complex data");
-    break;
-  default:
-    console.log("You entered a complex data");
-}
+     type = "boolean";
+   }                          
+  console.log(`You entered a ${type}`);//если никакие условия не отработают то останеться string по умолчанию
+                             
